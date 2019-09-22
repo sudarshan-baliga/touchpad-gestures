6e92d733b36d9b760f7c1a89fa373e38950a7cea
sudrshan baliga <baliga108@gmail.com>
2019-09-22 23:21:49 +0530

    1)fixed switch menu not getting displayed 2)  fixed initial trigger taking long time

--- main.c
+++ main.c
@@ -1,22 +1,35 @@
 #include <linux/input.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
 
 #define TOUCHPAD_FILE "/dev/input/event11"
+bool altDown;
+int TRIGGER_SMOOTHNESS = 30, RUNNING_SMOOTHNESS = 60, curSmoothness;
 
 void switchTabs(bool next)
 {
+        if (altDown)
+        {
+                system("xdotool key Tab");
+        }
+        else
+        {
+                system("xdotool keydown Alt");
+                altDown = true;
+                curSmoothness = RUNNING_SMOOTHNESS;
+        }
         printf("Switching to previous tab\n");
-        system("xdotool key Alt+Tab");
 }
 
 void handleTrackPad()
 {
-        int fd, smoothNess = 30, twoFingerEveCount;
+        int fd, twoFingerEveCount;
         bool twoFingers = false;
         struct input_event ie;
+        curSmoothness = TRIGGER_SMOOTHNESS;
         if ((fd = open(TOUCHPAD_FILE, O_RDONLY)) == -1)
         {
                 perror("Could not open the touchpad event file");
@@ -28,17 +41,20 @@ void handleTrackPad()
                 switch (ie.code)
                 {
                 case BTN_TOOL_FINGER:
-                        /* code */
                         printf("single Finger\n");
                         break;
                 case BTN_TOOL_DOUBLETAP:
-                        /* code */
                         twoFingers = !twoFingers;
                         if (!twoFingers)
+                        {
                                 twoFingerEveCount = 0;
+                                altDown = false;
+                                system("xdotool keyup Alt");
+                                curSmoothness = TRIGGER_SMOOTHNESS;
+                        }
                         break;
                 case ABS_X:
-                        if (twoFingers && twoFingerEveCount % smoothNess == 0)
+                        if (twoFingers && twoFingerEveCount % curSmoothness == 0)
                                 switchTabs(true);
                         twoFingerEveCount++;
                         break;
